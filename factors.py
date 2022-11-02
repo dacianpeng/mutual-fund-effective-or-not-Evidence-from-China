600fb4f3dc8b91507e15f82a6656e8322cbd5ddd
DacianPeng <dacianpeng@gmail.com>
2022-11-02 19:51:38 +0800

    temp stage

--- factors.py
+++ factors.py
@@ -14,7 +14,7 @@ from data.TwoStepData.main_data import main_data
 return_of_all_fund_ = return_of_all_fund.unstack(0)
 
 
-def group_and_statistic(α_matrix, groups = 5, weighting = 'mkt', interval = 1):
+def group_and_statistic(α_matrix, groups = 5, weighting = 'mkt', interval = 1, kind = 'cross_section', **other):
     '''
     weighting : string
 
@@ -23,6 +23,10 @@ def group_and_statistic(α_matrix, groups = 5, weighting = 'mkt', interval = 1):
     1/n, equal weighted
 
     interval : int
+
+    kind: cross_section or time_series
+
+    other: `time_span : int`
 
     the frequency to change holdings
     '''
@@ -63,28 +67,43 @@ def group_and_statistic(α_matrix, groups = 5, weighting = 'mkt', interval = 1):
         ]
         
     results = []
+    if kind == 'cross_section':
+        for regressor in regressors:
 
-    for regressor in regressors:
-
-        for group in range(groups):
-            temp = sm.OLS(RS_α[group] - RS_α.rf, RS_α[regressor]).fit()
-            monthly_α = temp.params[0]
-            # monthly_α = ((α + 1) ** (1 / interval)) - 1
+            for group in range(groups):
+                temp = sm.OLS(RS_α[group] - RS_α.rf, RS_α[regressor]).fit()
+                monthly_α = ((temp.params[0] + 1) ** (1 / interval)) - 1
+                results.append([float_to_percent(monthly_α), yearly_return(monthly_α), temp.tvalues[0].round(3), temp.rsquared.round(3), int(temp.nobs)])
+            # remove r_f
+            temp = sm.OLS(RS_α[groups - 1] - RS_α[0], RS_α[regressor]).fit()
+            monthly_α = ((temp.params[0] + 1) ** (1 / interval)) - 1
             results.append([float_to_percent(monthly_α), yearly_return(monthly_α), temp.tvalues[0].round(3), temp.rsquared.round(3), int(temp.nobs)])
-        # remove r_f
-        temp = sm.OLS(RS_α[groups - 1] - RS_α[0], RS_α[regressor]).fit()
-        monthly_α = temp.params[0]
-        # monthly_α = ((α + 1) ** (1 / interval)) - 1
-        results.append([float_to_percent(monthly_α), yearly_return(monthly_α), temp.tvalues[0].round(3), temp.rsquared.round(3), int(temp.nobs)])
 
-    excess_result, capm_α_result, svc_α_result = results[0: groups + 1], results[groups + 1: 2 * groups + 2], results[2 * groups + 2: 3 * groups + 3]
+        excess_result, capm_α_result, svc_α_result = results[0: groups + 1], results[groups + 1: 2 * groups + 2], results[2 * groups + 2: 3 * groups + 3]
+        columns = ['group' + str(i) for i in range(groups)] + ['long-short']
+
+    elif kind == 'time_series':
+        if len(other) != 0:
+            time_span = other['time_span']
+        else:
+            time_span = 36
+        for regressor in regressors:
+            for one_time_span in np.array_split(RS_α.index, len(RS_α.index) // time_span):
+                RS_α_ = RS_α.loc[one_time_span]
+                temp = sm.OLS(RS_α_[groups - 1] - RS_α_[0], RS_α_[regressor]).fit()
+                monthly_α = ((temp.params[0] + 1) ** (1 / interval)) - 1
+                results.append([float_to_percent(monthly_α), yearly_return(monthly_α), temp.tvalues[0].round(3), temp.rsquared.round(3), int(temp.nobs)])
+
+        excess_result, capm_α_result, svc_α_result = results[0: len(RS_α.index) // time_span], results[len(RS_α.index) // time_span: 2 * (len(RS_α.index) // time_span)], results[2 * (len(RS_α.index) // time_span): 3 *(len(RS_α.index) // time_span)]
+        columns = [f'{i[0]}/{i[-1]}' for i in np.array_split(RS_α.index, len(RS_α.index) // time_span)]
 
     excess_result = pd.DataFrame(np.array(excess_result).T, \
-        index=pd.MultiIndex.from_product([['excess'], ['α (monthly)', 'annual α', 't', 'R^2', 'Obs']]), columns=['group' + str(i) for i in range(groups)] + ['long-short'])
+        index=pd.MultiIndex.from_product([['excess'], ['α (monthly)', 'annual α', 't', 'R^2', 'Obs']]), columns=columns)
     capm_α_result = pd.DataFrame(np.array(capm_α_result).T, \
-        index=pd.MultiIndex.from_product([['capm α'], ['α (monthly)', 'annual α', 't', 'R^2', 'Obs']]), columns=['group' + str(i) for i in range(groups)] + ['long-short'])
+        index=pd.MultiIndex.from_product([['capm α'], ['α (monthly)', 'annual α', 't', 'R^2', 'Obs']]), columns=columns)
     svc_α_result = pd.DataFrame(np.array(svc_α_result).T, \
-        index=pd.MultiIndex.from_product([['svc α'], ['α (monthly)', 'annual α', 't', 'R^2', 'Obs']]), columns=['group' + str(i) for i in range(groups)] + ['long-short'])
+        index=pd.MultiIndex.from_product([['svc α'], ['α (monthly)', 'annual α', 't', 'R^2', 'Obs']]), columns=columns)
+
 
     return pd.concat([excess_result, capm_α_result, svc_α_result])
 
--- two_step_regression_source.py
+++ two_step_regression_source.py
@@ -1,4 +1,3 @@
-
 import pickle
 import pandas as pd
 
@@ -9,14 +8,14 @@ from data.TwoStepData.return_of_csmar_blend_fund import return_of_csmar_blend_fu
 from data.TwoStepData.return_of_csmar_stock_fund import return_of_csmar_stock_fund
 
 
-stock_fund_regression = ((return_of_csmar_stock_fund.unstack(level=0) - 1) * csmar_stock_fund_weight_yearly).sum(axis=1)
+stock_fund_regression = (return_of_csmar_stock_fund.unstack(level=0) * csmar_stock_fund_weight_yearly).sum(axis=1)
 stock_fund_regression.name = 'stock_fund'
 
-blend_fund_regression = ((return_of_csmar_blend_fund.unstack(level=0) - 1) * csmar_blend_fund_weight_yearly).sum(axis=1)
+blend_fund_regression = (return_of_csmar_blend_fund.unstack(level=0) * csmar_blend_fund_weight_yearly).sum(axis=1)
 blend_fund_regression.name = 'blend_fund'
 
 
-regression_source = pd.merge(stock_fund_regression, svc_source[['mktrf', 'rf', 'smb', 'vmg']], on='Date')
+regression_source = pd.merge(stock_fund_regression, svc_source[['mktrf', 'rf', 'smb', 'vmg']], on='Date').sort_index()
 regression_source = pd.merge(regression_source, blend_fund_regression, on='Date')
 
 regression_source['α'] = 1
